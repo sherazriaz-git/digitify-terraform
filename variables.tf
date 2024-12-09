eb58d536f5a9cb9bb7298cc78dac343d683cf7fa
sherazriaz-git <sherazriaz-git>
2024-12-09 23:41:14 +0500

    test

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "project_name" {
+  type    = string
+  default = "digitify"
+
+}
+
+variable "env" {
+  type    = string
+  default = "demo"
+
+}
+
+variable "maintainer" {
+  type = string
+
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+
+}
