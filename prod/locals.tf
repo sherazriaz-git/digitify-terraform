84c40fc1e48852c0cdcfebbba36c6d016e32da4d
sherazriaz-git <sherazriaz-git>
2024-12-10 14:20:00 +0500

    first commit

--- prod/locals.tf
+++ prod/locals.tf
@@ -1,17 +1,17 @@
-# locals {
-#   name = "${var.project_name}-${var.env}"
+locals {
+  name = "${var.project_name}-${var.env}"
 
-#   vpc = {
-#     enable_vpc_flow_logs = true
-#     vpc_cidr             = "10.0.0.0/16"
-#     enable_nat_gateway   = false
-#     azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
-#   }
+  vpc = {
+    enable_vpc_flow_logs = true
+    vpc_cidr             = "10.0.0.0/16"
+    enable_nat_gateway   = false
+    azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
+  }
 
-#   eks = {
+  eks = {
 
-#     version = "1.31"
+    version = "1.31"
 
 
-#   }
-# }
+  }
+}
